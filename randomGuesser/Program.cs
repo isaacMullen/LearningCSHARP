882b9f9ee69f517888fa9c1805da8fbea011cc37
isaacMullen <43121643+isaacMullen@users.noreply.github.com>
2024-07-20 22:01:23 -0300

    Add files via upload

---
+++ randomGuesser/Program.cs
@@ -0,0 +1,38 @@
+﻿Random random = new Random();
+
+while(true)
+{
+    int guessTracker = 0;
+
+    int upperRange = 1000000;
+    int lowerRange = 1;
+
+    int randomInteger = random.Next(lowerRange, upperRange);
+    int integerGuess = 0;
+
+    Console.WriteLine($"Int to guess: {randomInteger}");
+
+    integerGuess = (upperRange - lowerRange) / 2 + lowerRange;
+    Console.WriteLine($"Initial guess: {integerGuess}");
+    while (integerGuess != randomInteger)
+    {
+
+        if (integerGuess > randomInteger)
+        {
+            upperRange = integerGuess;
+            integerGuess = (upperRange - lowerRange) / 2 + lowerRange; 
+
+        }
+        else if (integerGuess < randomInteger)
+        {
+            lowerRange = integerGuess;
+            integerGuess = (upperRange - lowerRange) / 2 + lowerRange; 
+
+        } 
+        Console.WriteLine(integerGuess);
+        guessTracker++;
+    }
+    Console.WriteLine($"{guessTracker} guesses.");
+    Console.ReadKey();
+    Console.Clear();
+}
